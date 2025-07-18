947511d031f24dad4bd42168d06a322abfe75c59
Daniel Lopez <d@niel.to>
2025-07-17 19:53:12 -0700

    fix(book-search): remove immediate prop since no default commands anyway

--- src/components/book-search.tsx
+++ src/components/book-search.tsx
@@ -81,7 +81,7 @@ function CommandPalette({
         virtual={{
           options: filteredCommands,
         }}
-        immediate
+        // immediate
       >
         {({ activeOption }) => (
           <>
